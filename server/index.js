0d0e5164073f7c4a8fbe0a1cf8de47221b8dbf09
FriendlyIndr <abhyudaya.tiwari003@gmail.com>
2026-04-30 12:42:44 +0530

    Queue and worker added.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import multer from 'multer';
+import { Queue } from 'bullmq';
+
+const queue = new Queue('file-upload-queue');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -18,11 +21,20 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
-    return res.json({ status: "All good!" });
+  return res.json({ status: "All good!" });
 });
 
-app.post('/upload/pdf', upload.single('pdf'), (req, res) => {
-    return res.json({ message: 'uploaded' });
+app.post('/upload/pdf', upload.single('pdf'), async (req, res) => {
+  // Add job
+  await queue.add(
+    'file-ready', 
+    JSON.stringify({
+      filename: req.file.originalname,
+      destination: req.file.destination,
+      path: req.file.path,
+    }
+  ));
+  return res.json({ message: 'uploaded' });
 });
 
 app.listen(8000, () => console.log(`Server started on PORT:${8000}`));
